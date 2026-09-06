38bd7d64c672c8d838d1af99170a140b8fed4fc7
RED903 <65182037+RED903@users.noreply.github.com>
2026-09-06 21:17:48 +0900

    Add files via upload

---
+++ SlideShow/sw.js
@@ -0,0 +1,18 @@
+// SlideShow PWA Bypass Service Worker v1
+const CACHE_NAME = 'slideshow-pwa-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map(key => caches.delete(key)));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request).catch(() => new Response('오프라인입니다.')));
+});
